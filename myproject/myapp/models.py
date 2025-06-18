94919d7ba1e3c8fdb04fa1451f5f4a8742b715a6
tokenbug <tokenbug@gmail.com>
2025-06-18 17:28:46 +0700

    add author

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     description = models.TextField()
     published_at = models.DateField()
     
