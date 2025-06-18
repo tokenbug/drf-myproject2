21511302dfb623747bfc4912b6010ab553b69170
tokenbug <tokenbug@gmail.com>
2025-06-18 17:15:52 +0700

    urls

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('books/', BookListCreate.as_view(), name="book-list-create"),
     path('books/<int:pk>/', BookDeatail.as_view(), name="book-detail"),
     path('register/', Register.as_view(), name="register"),
+    path('user/', include('rest_framework.urls', namespace='rest_framework')),
 ]
