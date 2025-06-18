2a8ae58cdc2ddad6b5773eac8fe21bd918853005
tokenbug <41908782+tokenbug@users.noreply.github.com>
2025-06-18 17:37:07 +0700

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    price = models.Interger()
     description = models.TextField()
     published_at = models.DateField()
     
